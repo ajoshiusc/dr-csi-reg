953cc2658fd37ad476051d08cdf1fd25dfef1a28
Anand A. Joshi <ajoshi@usc.edu>
2025-03-22 01:29:27 -0700

    update

--- main_register2template.py
+++ main_register2template.py
@@ -1,6 +1,7 @@
 import os
 
 from registration import nonlin_register
+from multiprocessing import Pool
 
 template = "/deneb_disk/data_from_justin_03_11_2025/Data to be shared/Patient1/data/TE160_bval500.nii.gz"
 
@@ -10,14 +11,17 @@ data_dir = f"/deneb_disk/data_from_justin_03_11_2025/Data to be shared/{subject}
 all_te = [65, 80, 105, 130, 160, 200]
 
 for te in all_te:
-    for b in [0, 300, 500, 1000]:
+    def process_file(te_b):
+        te, b = te_b
         nii_file = f"{data_dir}/TE{te}_bval{b}.nii.gz"
 
         if os.path.exists(nii_file):
-
             nonlin_register(
                 moving=nii_file,
                 fixed=template,
                 output=nii_file.replace(".nii.gz", ".reg.nii.gz"),
             )
             print(f"{nii_file}.reg.nii.gz saved")
+
+    with Pool(5) as pool:
+        pool.map(process_file, [(te, b) for b in [0, 300, 500, 1000, 1500]])
