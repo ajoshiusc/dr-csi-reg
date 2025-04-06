f8710f7f8c821a5e634e23da9bd6e44a5abe3cc6
Anand Joshi <ajoshi@a01-04.hpc.usc.edu>
2025-04-05 19:35:07 -0700

    update

--- main_mat2nifti.py
+++ main_mat2nifti.py
@@ -13,9 +13,24 @@ import SimpleITK as sitk
 subject = "wip_patient2"
 res = [2.3, 2.3, 5]
 all_te = [63, 80, 105, 130, 160, 200, 300]
+
+data_dir = f'/project/ajoshi_27/data_from_justin_03_11_2025/Data to be shared/{subject}/data'
+
+# load the list of mat files and get the values of TEs from the file names
+all_te = []
+for filename in os.listdir(data_dir):
+    if filename.endswith('.mat'):
+        # extract the TE value from the filename
+        te = int(filename.split('TE')[1].split('.')[0])
+        all_te.append(te)
+all_te = sorted(all_te)
+#print all the TE values
+print('TE values:', all_te)
+
+
+
 for te in all_te:
 
-    data_dir = f'/deneb_disk/data_from_justin_03_11_2025/Data to be shared/{subject}/data'
     mat_file = f'{data_dir}/TE{te}.mat'
 
     mat = sio.loadmat(mat_file)
